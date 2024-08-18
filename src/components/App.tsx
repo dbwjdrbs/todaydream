00fb7c1e601fe1c2e6a69475355245a7a7ec4475
seoyeon1221 <rr124356@naver.com>
2024-08-18 19:25:03 +0900

    resultpage

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
-import '../styles/app.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'; // react-router-dom@^6.3.0
 import Home from '../pages/Home.tsx';
 import DreamInterpretation from '../pages/DreamInterpretation.tsx';
 import Header from './Header.tsx';
+import InterpretationResult from '../pages/InterpretationResult.tsx';
 
 const App = () => {
 
@@ -13,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/dream-interpretation" element={<DreamInterpretation/>}/>
+        <Route path='/interpretation-result' element={<InterpretationResult/>}/>
       </Routes>
     </BrowserRouter>
   );
